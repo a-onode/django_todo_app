5cf34cfd862706195a68f8664ea9a22238c120d2
a-onode <akt.onodera@gmail.com>
2025-03-26 21:32:47 +0900

    Todoモデルを作成

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    memo = models.TextField(null=True, blank=True)
+    due_at = models.DateField()
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
