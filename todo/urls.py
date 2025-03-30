3612f3ce9dd68572b229a5393b6f6814aa7bb5d4
a-onode <akt.onodera@gmail.com>
2025-03-30 09:07:35 +0900

    ステータス機能の実装

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import IndexTodo, CreateTodo, DeleteTodo
+from .views import IndexTodo, CreateTodo, DeleteTodo, change_status
 
 urlpatterns = [
     path('index/', IndexTodo.as_view(), name='index'),
     path('create/', CreateTodo.as_view(), name='create'),
     path('delete/<int:pk>/', DeleteTodo.as_view(), name='delete'),
+    path('status/<int:pk>/', change_status, name='status'),
 ]
